8a98bf3c79894050445455ce1b3069bbb7dccdb9
Drew Ryder <drewjryder@gmail.com>
2015-08-29 18:31:13 -0500

    Minor refactoring

--- autocropper/autocropper/autocropper.cpp
+++ autocropper/autocropper/autocropper.cpp
@@ -11,43 +11,12 @@ using namespace std;
 using namespace cv;
 using namespace utility;
 
-const string FILENAME_DELIMETER = "_";	// Files are expected to be named in the following format: [Prefix][Delimeter][Image Number].[File Extension]
+const string FILENAME_DELIMETER = "_";		// Files are expected to be named in the following format: [Prefix][Delimeter][Image Number].[File Extension]
 const int NUMBER_OF_DIGITS_IN_FILENAME = 3;	// Expected file names range from 001.png to 999.png
 
-vector<Mat> readDataset(const string& startingImageFilename)
-{
-	vector<Mat> images;
-
-	string filename = startingImageFilename;
-	string filenameSuffix = startingImageFilename.substr(startingImageFilename.find_last_of("."));
-	string filenamePrefix = startingImageFilename.substr(0, startingImageFilename.find_first_of(FILENAME_DELIMETER) + 1);
-	int fileNumber = 1;
-
-	//TODO_DR: Make this function more robust -- don't just read up to 72 and instead count how many files we will have with the specified prefix.
-	while (fileNumber <= 72)
-	{
-		if (!FileUtilities::fileExists(filename))
-		{
-			cerr << "File not found: " << filename << endl;
-		}
-		else
-		{
-			Mat image = imread(filename, CV_LOAD_IMAGE_GRAYSCALE);
-
-			images.push_back(image);
-		}
-
-		stringstream imageNumberStream;
-		fileNumber++;
-		imageNumberStream << setw(NUMBER_OF_DIGITS_IN_FILENAME) << setfill('0') << fileNumber;
-		filename = filenamePrefix + imageNumberStream.str() + filenameSuffix;
-		imageNumberStream.str("");
-	}
-
-	cout << "Number of images read: " << images.size() << endl;
-
-	return images;
-}
+string getFormattedFileNumber(int fileNumber);
+vector<Mat> readDataset(const string& startingImageFilename);
+void computeForegroundMask(const vector<Mat>& images);
 
 int main(int argc, char** argv)
 {
@@ -66,29 +35,81 @@ int main(int argc, char** argv)
 	}
 
 	vector<Mat> images = readDataset(argv[1]);
-	Mat fgMask, fgImg, bgImage;
-	BackgroundSubtractorMOG2 bg_model;
+
+	computeForegroundMask(images);
+
+	return EXIT_SUCCESS;
+}
+
+string getFormattedFileNumber(int fileNumber)
+{
+	stringstream imageNumberStream;
+
+	imageNumberStream << setw(NUMBER_OF_DIGITS_IN_FILENAME) << setfill('0') << fileNumber;
+
+	return imageNumberStream.str();
+}
+
+vector<Mat> readDataset(const string& startingImageFilename)
+{
+	vector<Mat> images;
+
+	string filename = startingImageFilename;
+	string filenameSuffix = startingImageFilename.substr(startingImageFilename.find_last_of("."));
+	string filenamePrefix = startingImageFilename.substr(0, startingImageFilename.find_first_of(FILENAME_DELIMETER) + 1);
+	int fileNumber = 1;	// Assume that our starting image starts at 1.
+
+	while (fileNumber <= 72)	//TODO_DR: Don't just read up to 72 and instead count how many files we will have with the specified prefix.
+	{
+		if (!FileUtilities::fileExists(filename))
+		{
+			cerr << "File not found: " << filename << endl;
+		}
+		else
+		{
+			Mat image = imread(filename, CV_LOAD_IMAGE_GRAYSCALE);
+
+			images.push_back(image);
+		}
+
+		filename = filenamePrefix + getFormattedFileNumber(++fileNumber) + filenameSuffix;
+	}
+
+	cout << "Number of images read: " << images.size() << endl;
+
+	return images;
+}
+
+void computeForegroundMask(const vector<Mat>& images)
+{
+	Mat foregroundMask, foregroundImage, backgroundImage;
+	BackgroundSubtractorMOG2 backgroundModel;
+
+	vector<Mat> foregroundMasks;
 
 	for each (Mat image in images)
 	{
-		if (fgImg.empty())
-			fgImg.create(image.size(), image.type());
+		if (foregroundImage.empty())
+			foregroundImage.create(image.size(), image.type());
 
-		bg_model(image, fgMask, -10);
+		backgroundModel(image, foregroundMask);
 
-		fgImg = Scalar::all(0);
-		image.copyTo(fgImg, fgMask);
-
-		//Mat bgImage;
-		//bg_model.getBackgroundImage(bgImage);
+		foregroundImage = Scalar::all(0);
+		image.copyTo(foregroundImage, foregroundMask);
 
 		waitKey();
 	}
 
-	imshow("fgMask", fgMask);
-	imshow("fgImg", fgImg);
+	//int erosion_size = 1;
+	//Mat element = getStructuringElement(MORPH_RECT,
+	//	Size(2 * erosion_size + 1, 2 * erosion_size + 1),
+	//	Point(erosion_size, erosion_size));
+	//erode(fgImg, fgImg, element);
 
+	imwrite("foreground.png", foregroundImage);
+
+	imshow("fgMask", foregroundMask);
+	imshow("fgImg", foregroundImage);
 
 	waitKey();
-	return EXIT_SUCCESS;
 }
