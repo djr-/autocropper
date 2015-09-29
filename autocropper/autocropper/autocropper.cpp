91057c1a3ddcc3244b191e99e8b1b8453935d117
Drew Ryder <drewjryder@gmail.com>
2015-09-28 20:36:07 -0500

    Move image reading related functions to their own class.

--- autocropper/autocropper/autocropper.cpp
+++ autocropper/autocropper/autocropper.cpp
@@ -1,21 +1,17 @@
 #include "file_utilities.h"
+#include "image_reader.h"
 #include <opencv2/core.hpp>
 #include <opencv2/highgui.hpp>
 #include <opencv2/imgproc.hpp>
 #include <opencv2/video.hpp>
 #include <iostream>
-#include <iomanip>
 #include <string>
 
+using namespace autocropper;
 using namespace std;
 using namespace cv;
 using namespace utility;
 
-const string FILENAME_DELIMETER = "_";		// Files are expected to be named in the following format: [Prefix][Delimeter][Image Number].[File Extension]
-const int NUMBER_OF_DIGITS_IN_FILENAME = 3;	// Expected file names range from 001.png to 999.png
-
-string getFormattedFileNumber(int fileNumber);
-vector<Mat> readDataset(const string& startingImageFilename);
 void computeForegroundMask(const vector<Mat>& images);
 
 int main(int argc, char** argv)
@@ -34,50 +30,11 @@ int main(int argc, char** argv)
 		return EXIT_FAILURE;
 	}
 
-	vector<Mat> images = readDataset(argv[1]);
+	vector<Mat> images = autocropper::ImageReader::readDataset(argv[1]);
 
 	computeForegroundMask(images);
 
 	return EXIT_SUCCESS;
-}
-
-string getFormattedFileNumber(int fileNumber)
-{
-	stringstream imageNumberStream;
-
-	imageNumberStream << setw(NUMBER_OF_DIGITS_IN_FILENAME) << setfill('0') << fileNumber;
-
-	return imageNumberStream.str();
-}
-
-vector<Mat> readDataset(const string& startingImageFilename)
-{
-	vector<Mat> images;
-
-	string filename = startingImageFilename;
-	string filenameSuffix = startingImageFilename.substr(startingImageFilename.find_last_of("."));
-	string filenamePrefix = startingImageFilename.substr(0, startingImageFilename.find_first_of(FILENAME_DELIMETER) + 1);
-	int fileNumber = 1;	// Assume that our starting image starts at 1.
-
-	while (fileNumber <= 72)	//TODO_DR: Don't just read up to 72 and instead count how many files we will have with the specified prefix.
-	{
-		if (!FileUtilities::fileExists(filename))
-		{
-			cerr << "File not found: " << filename << endl;
-		}
-		else
-		{
-			Mat image = imread(filename, CV_LOAD_IMAGE_GRAYSCALE);
-
-			images.push_back(image);
-		}
-
-		filename = filenamePrefix + getFormattedFileNumber(++fileNumber) + filenameSuffix;
-	}
-
-	cout << "Number of images read: " << images.size() << endl;
-
-	return images;
 }
 
 void computeForegroundMask(const vector<Mat>& images)
