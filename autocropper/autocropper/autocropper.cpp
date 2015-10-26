aed2cc75513151a191dfeae8fab8aea8ed6189f0
Drew Ryder <drewjryder@gmail.com>
2015-10-26 15:21:44 -0500

    Remove test code for otsu's method applied to the sobel image

--- autocropper/autocropper/autocropper.cpp
+++ autocropper/autocropper/autocropper.cpp
@@ -15,8 +15,6 @@ using namespace std;
 using namespace utility;
 using namespace OcvUtility;
 
-//Mat g_img;
-
 Mat trackbarMethod(Mat image, int sliderValue)
 {
 	Mat dst;
@@ -81,54 +79,15 @@ Mat preprocessImage(Mat img)
 	imwrite("TestImages/2highlightedContainer.png", drawRedRectOnImage(orig, containerRegion, 3));
 	Mat containerImage = img(containerRegion);
 
-	//threshold(containerImage, containerImage, 1, 255, THRESH_OTSU);
-	//imwrite("otsu_container.png", containerImage);
-	//Mat otsu_container = g_img(containerRegion).clone();
-	//imwrite("1container.png", otsu_container);
-	//Mat containerGrad = computeGradientImage(otsu_container).clone();
-	//imwrite("1containerGrad.png", containerGrad);
-	//imwrite("1grad_hist.png", plotHistogram(containerGrad));
-	//imwrite("1container_hist.png", plotHistogram(otsu_container));
-	//threshold(otsu_container, otsu_container, 1, 255, THRESH_OTSU);
-	//threshold(containerGrad, containerGrad, 1, 255, THRESH_OTSU);
-	//imwrite("1otsu_container.png", otsu_container);
-	//imwrite("1otsu_containerGrad.png", containerGrad);
-
 	Rect gelRegion = computeGelRegion(img(containerRegion));
 	Rect gelRegionWRTorig = Rect(containerRegion.x + gelRegion.x, containerRegion.y + gelRegion.y, gelRegion.width, gelRegion.height);
 	imwrite("TestImages/3highlightedGel.png", drawRedRectOnImage(orig, gelRegionWRTorig, 3));
 	Mat gelImage = containerImage(gelRegion);
 
-	//threshold(gelImage, gelImage, 1, 255, THRESH_OTSU);
-	//imwrite("otsu_gel.png", gelImage);
-	//Mat otsu_gel = g_img(gelRegionWRTorig).clone();
-	//imwrite("2gel.png", otsu_gel);
-	//Mat gelGrad = computeGradientImage(otsu_gel).clone();
-	//imwrite("2gelGrad.png", gelGrad);
-	//imwrite("2grad_hist.png", plotHistogram(gelGrad));
-	//imwrite("2gel_hist.png", experimental::plotHistogram(otsu_gel));
-	//threshold(otsu_gel, otsu_gel, 1, 255, THRESH_OTSU);
-	//threshold(gelGrad, gelGrad, 1, 255, THRESH_OTSU);
-	//imwrite("2otsu_gel.png", otsu_gel);
-	//imwrite("2otsu_gelGrad.png", gelGrad);
-
 	Rect rootRegion = computeRootRegion(gelImage);
 	Rect rootRegionWRTorig = Rect(containerRegion.x + gelRegion.x + rootRegion.x, containerRegion.y + gelRegion.y + rootRegion.y, rootRegion.width, rootRegion.height);
 	imwrite("TestImages/4highlightedroots.png", drawRedRectOnImage(orig, rootRegionWRTorig, 3));
 	Mat rootImage = gelImage(rootRegion);
-
-	//threshold(rootImage, rootImage, 1, 255, THRESH_OTSU);
-	//imwrite("otsu_root.png", rootImage);
-	//Mat otsu_root = g_img(rootRegionWRTorig).clone();
-	//imwrite("3root.png", otsu_root);
-	//Mat rootGrad = computeGradientImage(otsu_root).clone();
-	//imwrite("3rootGrad.png", rootGrad);
-	//imwrite("3grad_hist.png", plotHistogram(rootGrad));
-	//imwrite("3root_hist.png", experimental::plotHistogram(otsu_root));
-	//threshold(otsu_root, otsu_root, 1, 255, THRESH_OTSU);
-	//threshold(rootGrad, rootGrad, 1, 255, THRESH_OTSU);
-	//imwrite("3otsu_root.png", otsu_root);
-	//imwrite("3otsu_rootGrad.png", rootGrad);
 
 	//TODO: Compute the widest extents in the lower 3/4 of the image and then snap the left and right edges to that point?
 
@@ -152,19 +111,10 @@ int main(int argc, char** argv)
 	}
 
 	vector<Mat> images = ImageReader::readDataset(argv[1]);
-	//g_img = imread(argv[1], CV_LOAD_IMAGE_GRAYSCALE);	//TODO: Temporary mechanism to skip reading all of the images.
-
-	//Mat gradientImage = computeGradientImage(g_img).clone();
-	//threshold(gradientImage, gradientImage, 1, 255, THRESH_OTSU);
-	//imwrite("ORIG_otsu.png", gradientImage);
+	//Mat img = imread(argv[1], CV_LOAD_IMAGE_GRAYSCALE);	//TODO: Temporary mechanism to skip reading all of the images.
 
 	vector<Mat> foregroundImages = computeForegroundImages(images);
 	Mat orImg = or(foregroundImages);
-
-	//Mat gradientImage = computeGradientImage(orImg).clone();
-	//imwrite("or_grad.png", gradientImage);
-	//threshold(gradientImage, gradientImage, 1, 255, THRESH_OTSU);
-	//imwrite("or_otsu.png", gradientImage);
 
 	orImg = preprocessImage(orImg);
 
